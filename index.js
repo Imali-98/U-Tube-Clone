b1782bc989863d1f0ee74797a0f384624871fb23
Imali-98 <imalimadhubhashini98@gmail.com>
2023-06-25 11:57:25 +0530

    First Commit

---
+++ index.js
@@ -0,0 +1,20 @@
+import express from "express";
+import dotenv from "dotenv"
+import mongoose from "mongoose";
+
+dotenv.config()
+const app = express()
+
+const connectdb = () =>  {
+    mongoose.connect(process.env.MONGO).then(()=>{
+        console.log("connected to mongo db");
+    })
+    .catch((error)=>{
+        throw error
+    })
+}
+
+app.listen(8000, () => {
+    connectdb()
+    console.log("*****************");
+})  
---
+++ server/user/User.model.js
@@ -0,0 +1,31 @@
+import mongoose from "mongoose";
+
+const UserSchema = mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+        uniqe: true
+    },
+    email: {
+        type: String,
+        required: true,
+        uniqe: true
+    },
+    password: {
+        type: String,
+        required: true,
+    },
+    img: {
+        type: String,
+    },
+    subscribers: {
+        type: Number,
+        default: 0
+    },
+    subscriberUsers: {
+        type: Array,
+
+    },
+})
+
+export default mongoose.model("User", UserSchema)
