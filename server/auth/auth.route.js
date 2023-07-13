4bb6c88d46e9fe2a76d17e6306dd1381535576c3
Imali-98 <imalimadhubhashini98@gmail.com>
2023-07-13 10:15:48 +0530

    Code refactoring

--- server/auth/auth.route.js
+++ server/auth/auth.route.js
@@ -1,12 +1,12 @@
-import express from "express"
-import { signup } from "./auth.controller.js"
+import express from "express";
+import { signup } from "./auth.controller.js";
 
 const router = express.Router();
 
 router.post("/signup", signup);
 
-router.post("/signin",);
+router.post("/signin");
 
-router.post("/google",);
+router.post("/google");
 
-export default router 
+export default router;
