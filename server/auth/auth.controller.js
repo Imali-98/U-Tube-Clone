562b9c2bc9bbc375fec9f25b9dbb0744309d9617
Imali-98 <imalimadhubhashini98@gmail.com>
2023-07-13 10:30:34 +0530

    Code Refactoring

--- server/auth/auth.controller.js
+++ server/auth/auth.controller.js
@@ -1,10 +1,14 @@
 import mongoose from "mongoose";
-import User from "../user/User.model.js"
+import User from "../user/User.model.js";
+import bcrypt from "bcryptjs";
 
 export const signup = async (req, res) => {
-  // try {
-  //   const newUser = new User();
-  // } catch (error) {}
-  console.log(req.body);
+  try {
+    var salt = bcrypt.genSaltSync(10);
+    var hash = bcrypt.hashSync(req.body.password, salt);
+    const newUser = new User(...req.body, password);
+
+    await newUser.save();
+    res.status(200).send("User has been created!")
+  } catch (error) {}
 };
- 
