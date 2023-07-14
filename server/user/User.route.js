bad20b2f527787576929f12ae67e14f966d5e9bf
Imali-98 <imalimadhubhashini98@gmail.com>
2023-07-14 20:05:12 +0530

    committed

--- server/user/User.route.js
+++ server/user/User.route.js
@@ -1,11 +1,11 @@
 import express from "express"
 import { deleteUser, dislike, getUser, like, subscribe, unsuscribe, update } from "./User.controller.js"
-import { verifyToken } from "../../verifyToken.js";
+// import { verifyToken } from "../../verifyToken.js";
 
 const router = express.Router();
 
 // update user
-router.put('/:id',verifyToken, update)
+// router.put('/:id',verifyToken, update)
 
 //delete user
 router.delete('/:id', deleteUser)
