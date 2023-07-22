c2fbbf912266191c39d41474e931987ff079426b
Imali-98 <imalimadhubhashini98@gmail.com>
2023-07-22 23:07:16 +0530

    commit

--- server/user/User.route.js
+++ server/user/User.route.js
@@ -5,24 +5,24 @@ import { verifyToken } from "../../verifyToken.js";
 const router = express.Router();
 
 // update user
-router.put('/:id', update)
+// router.put('/:id', update)
 
-//delete user
-router.delete('/:id', deleteUser)
+// //delete user
+// router.delete('/:id', deleteUser)
 
-//get a user
-router.get('find/:id', getUser)
+// //get a user
+// router.get('find/:id', getUser)
  
-//subscribe a user
-router.put('sub/:id', subscribe)
+// //subscribe a user
+// router.put('sub/:id', subscribe)
 
-//unsubscribe a user
-router.put('unsub/:id', unsubscribe)
+// //unsubscribe a user
+// router.put('unsub/:id', unsubscribe)
 
-//like a video
-router.put('like/:videoId', like)
+// //like a video
+// router.put('like/:videoId', like)
 
-//dislike a video
-router.put('dislike/:videoId', dislike)
+// //dislike a video
+// router.put('dislike/:videoId', dislike)
 
 export default router 
