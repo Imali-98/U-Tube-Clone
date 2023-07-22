3c1c55ff3d2d9eb62b1bc482af3788d89202f5bd
Imali-98 <imalimadhubhashini98@gmail.com>
2023-07-22 22:49:56 +0530

    commit

--- server/video/Video.controller.js
+++ server/video/Video.controller.js
@@ -10,22 +10,22 @@ export const addVideo = async (req, res, next) => {
   }
 };
 export const updateVideo = async (req, res, next) => {
-  // try {
-  //   const video = await Video.findById(req.params.id);
-  //   if (!video) return next(createError(404, "User not found"));
-  //   if (req.user.id === video.userId) {
-  //     const updatedVideo = await User.findByIdAndUpdate(
-  //       req.params.id,
-  //       {
-  //         $set: req.body,
-  //       },
-  //       { new: true }
-  //     );
-  //     res.status(200).json(updatedVideo);
-  //   } else {
-  //     return next(createError(403, "U can only update your video!"));
-  //   }
-  // } catch {}
+  try {
+    const video = await Video.findById(req.params.id);
+    if (!video) return next(createError(404, "User not found"));
+    if (req.user.id === video.userId) {
+      const updatedVideo = await User.findByIdAndUpdate(
+        req.params.id,
+        {
+          $set: req.body,
+        },
+        { new: true }
+      );
+      res.status(200).json(updatedVideo);
+    } else {
+      return next(createError(403, "U can only update your video!"));
+    }
+  } catch {}
 };
 export const deleteVideo = async (req, res, next) => {
   try {
