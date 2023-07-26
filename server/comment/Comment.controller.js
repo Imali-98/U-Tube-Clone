bbe953fa2001aa61740ece2a76c8347b18477139
Imali-98 <imalimadhubhashini98@gmail.com>
2023-07-26 23:15:46 +0530

    commit

--- server/comment/Comment.controller.js
+++ server/comment/Comment.controller.js
@@ -1,6 +1,6 @@
 import { createError } from "../../error.js";
-import Video from "../video/Video.model.js"
-import Comment from "./Comment.model.js"
+import Video from "../video/Video.model.js";
+import Comment from "./Comment.model.js";
 
 export const addComment = async (req, res, next) => {
   const newComment = new Comment({ ...req.body, userId: req.body.id });
@@ -14,13 +14,13 @@ export const addComment = async (req, res, next) => {
 
 export const deleteComment = async (req, res, next) => {
   try {
-    const comment = await Comment.findById(res.params.id)
-    const video = await Video.findById(res.params.id)
-    if(req.user.id === comment.userId || req.user.id === video.userId){
-        await Comment.findByIdAndDelete(res.params.id)
-        res.status(200).json("The comment has been deleted!");
-    }else{
-        return next(createError(403,"You can only delete your comment"))
+    const comment = await Comment.findById(res.params.id);
+    const video = await Video.findById(res.params.id);
+    if (req.user.id === comment.userId || req.user.id === video.userId) {
+      await Comment.findByIdAndDelete(res.params.id);
+      res.status(200).json("The comment has been deleted!");
+    } else {
+      return next(createError(403, "You can only delete your comment"));
     }
   } catch (err) {
     next(err);
@@ -29,7 +29,7 @@ export const deleteComment = async (req, res, next) => {
 
 export const getComment = async (req, res, next) => {
   try {
-    const comments = await Comment.find({videoId:req.params.videoId});
+    const comments = await Comment.find({ videoId: req.params.videoId });
     res.status(200).json(comments);
   } catch (err) {
     next(err);
