becf7071d634a737eb0d880be8928e17859af159
Imali-98 <imalimadhubhashini98@gmail.com>
2023-07-26 23:11:59 +0530

    committed

--- server/video/Video.route.js
+++ server/video/Video.route.js
@@ -4,16 +4,16 @@ import { verifyToken } from "../../verifyToken.js";
 
 const router = express.Router();
 
-router.post("/",verifyToken, addVideo)
-router.put("/:id",verifyToken, updateVideo)
-router.delete("/:id",verifyToken, deleteVideo)
-router.get("/find/:id", getVideo)
-router.put("/view/:id", addView)
-router.get("/trend", trend)
-router.get("/random", random)
-router.get("/sub",verifyToken, sub)
-router.get("/tags", getByTag)
-router.get("/search", search)
+// router.post("/",verifyToken, addVideo)
+// router.put("/:id",verifyToken, updateVideo)
+// router.delete("/:id",verifyToken, deleteVideo)
+// router.get("/find/:id", getVideo)
+// router.put("/view/:id", addView)
+// router.get("/trend", trend)
+// router.get("/random", random)
+// router.get("/sub",verifyToken, sub)
+// router.get("/tags", getByTag)
+// router.get("/search", search)
 
 
 export default router 
