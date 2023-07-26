17ff233d2238125db57bd0514adb383256e0f8e2
Imali-98 <imalimadhubhashini98@gmail.com>
2023-07-26 23:13:34 +0530

    committed

--- server/comment/Comment.controller.js
+++ server/comment/Comment.controller.js
@@ -13,18 +13,18 @@ export const addComment = async (req, res, next) => {
 };
 
 export const deleteComment = async (req, res, next) => {
-  try {
-    const comment = await Comment.findById(res.params.id)
-    const video = await Video.findById(res.params.id)
-    if(req.user.id === comment.userId || req.user.id === video.userId){
-        await Comment.findByIdAndDelete(res.params.id)
-        res.status(200).json("The comment has been deleted!");
-    }else{
-        return next(createError(403,"You can only delete your comment"))
-    }
-  } catch (err) {
-    next(err);
-  }
+  // try {
+  //   const comment = await Comment.findById(res.params.id)
+  //   const video = await Video.findById(res.params.id)
+  //   if(req.user.id === comment.userId || req.user.id === video.userId){
+  //       await Comment.findByIdAndDelete(res.params.id)
+  //       res.status(200).json("The comment has been deleted!");
+  //   }else{
+  //       return next(createError(403,"You can only delete your comment"))
+  //   }
+  // } catch (err) {
+  //   next(err);
+  // }
 };
 
 export const getComment = async (req, res, next) => {
