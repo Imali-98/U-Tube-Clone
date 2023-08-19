c5d6ca4479187fbc1c27162076729172027e8479
Imali-98 <imalimadhubhashini98@gmail.com>
2023-08-19 18:20:04 +0530

    Add googleAuth function to auth.js file

--- server/auth/auth.controller.js
+++ server/auth/auth.controller.js
@@ -2,38 +2,60 @@ import mongoose from "mongoose";
 import User from "../user/User.model.js";
 import bcrypt from "bcryptjs";
 import { createError } from "../../error.js";
-import jwt from "jsonwebtoken"
+import jwt from "jsonwebtoken";
 
 export const signup = async (req, res, next) => {
   try {
     var salt = bcrypt.genSaltSync(10);
     var hash = bcrypt.hashSync(req.body.password, salt);
-    const newUser = new User({...req.body, password: hash});
+    const newUser = new User({ ...req.body, password: hash });
 
     await newUser.save();
-    res.status(200).send("User has been created!")
+    res.status(200).send("User has been created!");
   } catch (error) {
-    next(createError(404, "not found sorry!"))
+    next(createError(404, "not found sorry!"));
   }
 };
 
 export const signin = async (req, res, next) => {
   try {
-    const user = await User.findOne({name:req.body.name})
-    if (!user) return next(createError(404,"User not found"))
+    const user = await User.findOne({ name: req.body.name });
+    if (!user) return next(createError(404, "User not found"));
 
-    const isCorrect = await bcrypt.compare(req.body.password, user.password)
-    if (!isCorrect)  return next(createError(400,"Wrong Credentials!"))
+    const isCorrect = await bcrypt.compare(req.body.password, user.password);
+    if (!isCorrect) return next(createError(400, "Wrong Credentials!"));
 
-    const token = jwt.sign({id:user._id}, process.env.JWT)
-    const { password, ...others } = user._doc
+    const token = jwt.sign({ id: user._id }, process.env.JWT);
+    const { password, ...others } = user._doc;
 
-    res.cookie("access_token", token,{
-      httpOnly: true
-    }).status(200)
-    .json(others)
-
+    res
+      .cookie("access_token", token, {
+        httpOnly: true,
+      })
+      .status(200)
+      .json(others);
   } catch (error) {
-    next(error)
+    next(error);
   }
+};
+
+export const googleAuth = async (res, req, next) => {
+  try {
+    const user = await User.findOne({ email: req.body.email });
+    if (user) {
+      const token = jwt.sign({ id: user._id }, process.env.JWT);
+      res
+        .cookie("access_token", token, {
+          httpOnly: true,
+        })
+        .status(200)
+        .json(user._doc);
+    }else{
+      const newUser = new User({
+        ...req.body,
+        fromGoogle:true
+        
+      })
+    }
+  } catch (err) {}
 };
